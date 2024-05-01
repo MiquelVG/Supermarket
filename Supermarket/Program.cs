ae350c4a1ce804739fdca3378a6327c115a5bd75
MiquelVidal <a2308927@institutmontilivi.cat>
2024-05-01 18:34:55 +0200

    Classe Customer

--- Supermarket/Program.cs
+++ Supermarket/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Supermarket
+﻿using System.Text;
+
+namespace Supermarket
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = Encoding.UTF8;
+            Customer miquel = new Customer("724891D","Miquel Vidal Gomila",25);
+            Console.WriteLine(miquel.FullName);
+            miquel.AddInvoicedAmount(44.3);
+            Console.WriteLine(miquel.GetRating);
+            miquel.Active = true;
+            miquel.AddPoints(34);
+
+            Console.WriteLine(miquel);
         }
     }
 }
